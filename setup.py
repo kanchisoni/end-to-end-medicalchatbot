87dbfbd53cec558aeb46a106d664d869292d03b3
kanchisoni <kanchisoni2911@gmail.com>
2025-04-07 17:15:54 +0530

    folders added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'ks',
+    author_email= 'kanchisoni2911@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
